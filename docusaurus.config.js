03df7a067ccfc942ffb095f2cb80610b4bb5f286
rkaron <67106617+rkaron@users.noreply.github.com>
2021-03-17 08:54:36 +0100

    Pt 2928 (#1)
    * Applied docs-only style
    
    * Applied requested colors in CSS
    
    * Improved notes and blockquote colors
    
    * Updates per request from February 26
    
    * Improved colors
    
    * 05.03.2021 updates

---
+++ docusaurus.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  title: 'NavVis',
+  tagline: 'NavVis Webhelp',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.svg',
+  organizationName: 'NavVis', // Usually your GitHub org/user name.
+  projectName: 'navvis-docusaurus-template', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'NavVis',
+      logo: {
+        alt: 'NavVis Logo',
+        src: 'img/NavVis_Logo_Digital_White.png',
+      },
+    },
+    hideableSidebar: true, // Remove to hide the left pane
+    //sidebarCollapsible: false, // Remove to disable the left-pane collapse/expand button
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} NavVis, Inc.`,
+    },
+    // The following determines the dark/light mode switch styling
+    colorMode: {
+      respectPrefersColorScheme: true,
+      switchConfig: {
+        lightIcon: "\u{25CB}",
+        darkIcon: "\u{25CF}",
+        darkIconStyle: {
+          color: `#f4f4f4`,
+        },
+        lightIconStyle: {
+          color: `#f4f4f4`,
+        },
+      },
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        blog: false,
+        pages: false,
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          path: "./docs",
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
