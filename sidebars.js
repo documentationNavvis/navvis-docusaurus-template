c7a8b98ecb108aaf268b565a8a20d13f5a535405
RKaron(jorsek) <rafal.karon@Jorsek.com>
2021-04-22 15:13:33 +0200

    removed redundant content

--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: [
-      "0cf92ab0-78d3-11ea-bc51-02429399f122",
-      "doc1",
-      "doc2",
-      "doc3",
-    ],
+    Docusaurus: ["doc1", "doc2", "doc3"],
     Features: ["mdx"],
   },
 };
