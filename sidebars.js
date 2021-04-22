81651a9750dcffe9aa1515a7555eb88cc5740646
RKaron(jorsek) <rafal.karon@Jorsek.com>
2021-04-22 15:08:28 +0200

    fixed code background for notes

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,11 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    Docusaurus: [
+      "0cf92ab0-78d3-11ea-bc51-02429399f122",
+      "doc1",
+      "doc2",
+      "doc3",
+    ],
+    Features: ["mdx"],
   },
 };
